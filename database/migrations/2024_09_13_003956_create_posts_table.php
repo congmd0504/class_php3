a668f2e5d3ccbb82b36d8c9a4da34a7087fbb2f3
congmd0504 <congmd0504@gmail.com>
2024-10-02 09:09:38 +0700

    qll

--- database/migrations/2024_09_13_003956_create_posts_table.php
+++ database/migrations/2024_09_13_003956_create_posts_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('image');
             $table->text('description');
             $table->longText('content');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
